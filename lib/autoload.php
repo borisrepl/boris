b0d2461625da6ea29489dd2ef6439f2268e2afc0
d11wtq <chris@w3style.co.uk>
2013-03-03 11:47:31 +1100

    Forgot expandtab in vim

--- lib/autoload.php
+++ lib/autoload.php
@@ -4,13 +4,13 @@
  * Custom autoloader for non-composer installations.
  */
 spl_autoload_register(function($class) {
-	if ($class[0] == '\\') {
-		$class = substr($class, 1);
-	}
+  if ($class[0] == '\\') {
+    $class = substr($class, 1);
+  }
 
-	$path = sprintf('%s/%s.php', __DIR__, implode('/', explode('\\', $class)));
+  $path = sprintf('%s/%s.php', __DIR__, implode('/', explode('\\', $class)));
 
-	if (is_file($path)) {
-		require_once($path);
-	}
+  if (is_file($path)) {
+    require_once($path);
+  }
 });
