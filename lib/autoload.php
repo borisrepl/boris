0fd78220e5a572142803e68fc0484886168f2c80
Tejas Manohar <me@tejas.io>
2015-02-28 16:14:50 -0600

    clean autoload.php

--- lib/autoload.php
+++ lib/autoload.php
@@ -5,14 +5,15 @@
 /**
  * Custom autoloader for non-composer installations.
  */
-spl_autoload_register(function($class) {
-  if ($class[0] == '\\') {
-    $class = substr($class, 1);
-  }
-
-  $path = sprintf('%s/%s.php', __DIR__, implode('/', explode('\\', $class)));
-
-  if (is_file($path)) {
-    require_once($path);
-  }
+spl_autoload_register(function($class)
+{
+    if ($class[0] == '\\') {
+        $class = substr($class, 1);
+    }
+    
+    $path = sprintf('%s/%s.php', __DIR__, implode('/', explode('\\', $class)));
+    
+    if (is_file($path)) {
+        require_once($path);
+    }
 });
