c9db6da48f765f103048d5c38e4b10fa4dce579c
Tejas Manohar <me@tejas.io>
2015-02-28 16:19:10 -0600

    clean Inspector.php

--- lib/Boris/Inspector.php
+++ lib/Boris/Inspector.php
@@ -7,13 +7,14 @@ namespace Boris;
 /**
  * Something that is capable of returning a useful representation of a variable.
  */
-interface Inspector {
-  /**
-   * Return a debug-friendly string representation of $variable.
-   *
-   * @param mixed $variable
-   *
-   * @return string
-   */
-  public function inspect($variable);
+interface Inspector
+{
+    /**
+     * Return a debug-friendly string representation of $variable.
+     *
+     * @param mixed $variable
+     *
+     * @return string
+     */
+    public function inspect($variable);
 }
