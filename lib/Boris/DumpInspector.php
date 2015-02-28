140da7ab50984d1c97b2aa144025edf29121a899
Tejas Manohar <me@tejas.io>
2015-02-28 16:17:44 -0600

    clean DumpInspector.php

--- lib/Boris/DumpInspector.php
+++ lib/Boris/DumpInspector.php
@@ -7,10 +7,12 @@ namespace Boris;
 /**
  * Passes values through var_dump() to inspect them.
  */
-class DumpInspector implements Inspector {
-  public function inspect($variable) {
-    ob_start();
-    var_dump($variable);
-    return sprintf(" → %s", trim(ob_get_clean()));
-  }
+class DumpInspector implements Inspector
+{
+    public function inspect($variable)
+    {
+        ob_start();
+        var_dump($variable);
+        return sprintf(" → %s", trim(ob_get_clean()));
+    }
 }
