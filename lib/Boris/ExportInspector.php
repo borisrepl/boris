c1bc631d22c132e44c29efafb3a0cec660402c0b
Tejas Manohar <me@tejas.io>
2015-02-28 16:18:49 -0600

    clean ExportInspector.php

--- lib/Boris/ExportInspector.php
+++ lib/Boris/ExportInspector.php
@@ -7,8 +7,10 @@ namespace Boris;
 /**
  * Passes values through var_export() to inspect them.
  */
-class ExportInspector implements Inspector {
-  public function inspect($variable) {
-    return sprintf(" → %s", var_export($variable, true));
-  }
+class ExportInspector implements Inspector
+{
+    public function inspect($variable)
+    {
+        return sprintf(" → %s", var_export($variable, true));
+    }
 }
