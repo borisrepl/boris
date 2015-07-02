af4a7ea35935a003bd18af537b76dabdbf458842
Bradley Wogsland <bwogsland@leadspedia.com>
2015-07-01 20:08:35 -0500

    stubs

---
+++ tests/lib/Boris/ConfigTest.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This class tests lib/autoload.php
+ *
+ * To run this individual test:
+ *
+ * phpunit --bootstrap tests/autoload.php tests/lib/Boris/ConfigTest --coverage-html ./reports
+ *
+ */
+namespace tests\lib\Boris;
+use \Boris\Config as Config;
+
+class ConfigTest
+extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests the constructor.
+     */
+    public function test_constructor () {
+
+    }
+
+    /**
+     * Tests the apply method.
+     */
+    public function test_apply () {
+
+    }
+
+    /**
+     * Tests the loadedFiles method.
+     */
+    public function test_loadedFiles () {
+
+    }
+}
